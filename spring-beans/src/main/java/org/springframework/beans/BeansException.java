4261fbb7a5071fc7cb481fe516751986249d9286
xinxing <xinguimeng@163.com>
2017-12-28 01:10:39 +0800

    BeansException注释

--- spring-beans/src/main/java/org/springframework/beans/BeansException.java
+++ spring-beans/src/main/java/org/springframework/beans/BeansException.java
@@ -26,47 +26,57 @@ import org.springframework.util.ObjectUtils;
  * <p>Note that this is a runtime (unchecked) exception. Beans exceptions
  * are usually fatal; there is no reason for them to be checked.
  *
+ * 在所有的beans包和子包中抛出的异常类的抽象父类
+ * Beans异常通常都是严重错误
+ * 因此没有理由让他们成为受检异常，成为运行时异常即可
+ *
  * @author Rod Johnson
  * @author Juergen Hoeller
  */
 @SuppressWarnings("serial")
 public abstract class BeansException extends NestedRuntimeException {
 
-	/**
-	 * Create a new BeansException with the specified message.
-	 * @param msg the detail message
-	 */
-	public BeansException(String msg) {
-		super(msg);
-	}
+    /**
+     * Create a new BeansException with the specified message.
+     *
+     * 构造方法
+     *
+     * @param msg the detail message
+     */
+    public BeansException(String msg) {
+        super(msg);
+    }
 
-	/**
-	 * Create a new BeansException with the specified message
-	 * and root cause.
-	 * @param msg the detail message
-	 * @param cause the root cause
-	 */
-	public BeansException(String msg, Throwable cause) {
-		super(msg, cause);
-	}
+    /**
+     * Create a new BeansException with the specified message
+     * and root cause.
+     *
+     * 构造方法
+     *
+     * @param msg the detail message
+     * @param cause the root cause
+     */
+    public BeansException(String msg, Throwable cause) {
+        super(msg, cause);
+    }
 
 
-	@Override
-	public boolean equals(Object other) {
-		if (this == other) {
-			return true;
-		}
-		if (!(other instanceof BeansException)) {
-			return false;
-		}
-		BeansException otherBe = (BeansException) other;
-		return (getMessage().equals(otherBe.getMessage()) &&
-				ObjectUtils.nullSafeEquals(getCause(), otherBe.getCause()));
-	}
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+        if (!(other instanceof BeansException)) {
+            return false;
+        }
+        BeansException otherBe = (BeansException) other;
+        return (getMessage().equals(otherBe.getMessage()) &&
+                ObjectUtils.nullSafeEquals(getCause(), otherBe.getCause()));
+    }
 
-	@Override
-	public int hashCode() {
-		return getMessage().hashCode();
-	}
+    @Override
+    public int hashCode() {
+        return getMessage().hashCode();
+    }
 
 }
