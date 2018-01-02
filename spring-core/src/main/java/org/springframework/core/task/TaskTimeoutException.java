6fdbbb1c11441f055f42dac250a1d4efe79a75f4
xinxing <xinguimeng@163.com>
2018-01-02 19:29:46 +0800

    task对应的异常

--- spring-core/src/main/java/org/springframework/core/task/TaskTimeoutException.java
+++ spring-core/src/main/java/org/springframework/core/task/TaskTimeoutException.java
@@ -20,6 +20,8 @@ package org.springframework.core.task;
  * Exception thrown when a {@link AsyncTaskExecutor} rejects to accept
  * a given task for execution because of the specified timeout.
  *
+ * 任务超时异常
+ *
  * @author Juergen Hoeller
  * @since 2.0.3
  * @see AsyncTaskExecutor#execute(Runnable, long)
@@ -28,25 +30,31 @@ package org.springframework.core.task;
 @SuppressWarnings("serial")
 public class TaskTimeoutException extends TaskRejectedException {
 
-	/**
-	 * Create a new {@code TaskTimeoutException}
-	 * with the specified detail message and no root cause.
-	 * @param msg the detail message
-	 */
-	public TaskTimeoutException(String msg) {
-		super(msg);
-	}
+    /**
+     * Create a new {@code TaskTimeoutException}
+     * with the specified detail message and no root cause.
+     *
+     * 构造方法
+     *
+     * @param msg the detail message
+     */
+    public TaskTimeoutException(String msg) {
+        super(msg);
+    }
 
-	/**
-	 * Create a new {@code TaskTimeoutException}
-	 * with the specified detail message and the given root cause.
-	 * @param msg the detail message
-	 * @param cause the root cause (usually from using an underlying
-	 * API such as the {@code java.util.concurrent} package)
-	 * @see java.util.concurrent.RejectedExecutionException
-	 */
-	public TaskTimeoutException(String msg, Throwable cause) {
-		super(msg, cause);
-	}
+    /**
+     * Create a new {@code TaskTimeoutException}
+     * with the specified detail message and the given root cause.
+     *
+     * 构造方法
+     *
+     * @param msg the detail message
+     * @param cause the root cause (usually from using an underlying
+     * API such as the {@code java.util.concurrent} package)
+     * @see java.util.concurrent.RejectedExecutionException
+     */
+    public TaskTimeoutException(String msg, Throwable cause) {
+        super(msg, cause);
+    }
 
 }
