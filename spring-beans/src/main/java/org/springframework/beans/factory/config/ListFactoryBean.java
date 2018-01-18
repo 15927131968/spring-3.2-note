d71f40cf83459b6d731009ce0dd31ed0398b7340
xinxing <xinguimeng@163.com>
2018-01-18 14:34:30 +0800

    list、map、set三类具体的工厂Bean

--- spring-beans/src/main/java/org/springframework/beans/factory/config/ListFactoryBean.java
+++ spring-beans/src/main/java/org/springframework/beans/factory/config/ListFactoryBean.java
@@ -27,6 +27,8 @@ import org.springframework.core.GenericCollectionTypeResolver;
  * Simple factory for shared List instances. Allows for central setup
  * of Lists via the "list" element in XML bean definitions.
  *
+ * 列表工厂Bean
+ *
  * @author Juergen Hoeller
  * @since 09.12.2003
  * @see SetFactoryBean
@@ -34,67 +36,92 @@ import org.springframework.core.GenericCollectionTypeResolver;
  */
 public class ListFactoryBean extends AbstractFactoryBean<List> {
 
-	private List sourceList;
+    /**
+     * 源列表
+     */
+    private List sourceList;
 
-	private Class targetListClass;
-
-
-	/**
-	 * Set the source List, typically populated via XML "list" elements.
-	 */
-	public void setSourceList(List sourceList) {
-		this.sourceList = sourceList;
-	}
-
-	/**
-	 * Set the class to use for the target List. Can be populated with a fully
-	 * qualified class name when defined in a Spring application context.
-	 * <p>Default is a {@code java.util.ArrayList}.
-	 * @see java.util.ArrayList
-	 */
-	public void setTargetListClass(Class targetListClass) {
-		if (targetListClass == null) {
-			throw new IllegalArgumentException("'targetListClass' must not be null");
-		}
-		if (!List.class.isAssignableFrom(targetListClass)) {
-			throw new IllegalArgumentException("'targetListClass' must implement [java.util.List]");
-		}
-		this.targetListClass = targetListClass;
-	}
+    /**
+     * 目标列表类
+     */
+    private Class targetListClass;
 
 
-	@Override
-	public Class<List> getObjectType() {
-		return List.class;
-	}
+    /**
+     * Set the source List, typically populated via XML "list" elements.
+     *
+     * 设置源列表
+     */
+    public void setSourceList(List sourceList) {
+        this.sourceList = sourceList;
+    }
 
-	@Override
-	@SuppressWarnings("unchecked")
-	protected List createInstance() {
-		if (this.sourceList == null) {
-			throw new IllegalArgumentException("'sourceList' is required");
-		}
-		List result = null;
-		if (this.targetListClass != null) {
-			result = (List) BeanUtils.instantiateClass(this.targetListClass);
-		}
-		else {
-			result = new ArrayList(this.sourceList.size());
-		}
-		Class valueType = null;
-		if (this.targetListClass != null) {
-			valueType = GenericCollectionTypeResolver.getCollectionType(this.targetListClass);
-		}
-		if (valueType != null) {
-			TypeConverter converter = getBeanTypeConverter();
-			for (Object elem : this.sourceList) {
-				result.add(converter.convertIfNecessary(elem, valueType));
-			}
-		}
-		else {
-			result.addAll(this.sourceList);
-		}
-		return result;
-	}
+    /**
+     * Set the class to use for the target List. Can be populated with a fully
+     * qualified class name when defined in a Spring application context.
+     * <p>Default is a {@code java.util.ArrayList}.
+     *
+     * 设置目标列表对象
+     * 默认为java.util.ArrayList
+     *
+     * @see java.util.ArrayList
+     */
+    public void setTargetListClass(Class targetListClass) {
+        if (targetListClass == null) {
+            throw new IllegalArgumentException("'targetListClass' must not be null");
+        }
+        if (!List.class.isAssignableFrom(targetListClass)) {
+            throw new IllegalArgumentException("'targetListClass' must implement [java.util.List]");
+        }
+        this.targetListClass = targetListClass;
+    }
+
+
+    /**
+     * 获取目标类型
+     *
+     * @return
+     */
+    @Override
+    public Class<List> getObjectType() {
+        return List.class;
+    }
+
+    /**
+     * 创建实例
+     *
+     * @return
+     */
+    @Override
+    @SuppressWarnings("unchecked")
+    protected List createInstance() {
+        // 源list类型不能为空
+        if (this.sourceList == null) {
+            throw new IllegalArgumentException("'sourceList' is required");
+        }
+
+        // 实例化目标list
+        List result = null;
+        if (this.targetListClass != null) {
+            result = (List) BeanUtils.instantiateClass(this.targetListClass);
+        } else {
+            result = new ArrayList(this.sourceList.size());
+        }
+
+        // 是否需要类型转换
+        Class valueType = null;
+        if (this.targetListClass != null) {
+            valueType = GenericCollectionTypeResolver.getCollectionType(this.targetListClass);
+        }
+        if (valueType != null) {
+            TypeConverter converter = getBeanTypeConverter();
+            for (Object elem : this.sourceList) {
+                result.add(converter.convertIfNecessary(elem, valueType));
+            }
+        } else {
+            result.addAll(this.sourceList);
+        }
+        return result;
+    }
 
 }
